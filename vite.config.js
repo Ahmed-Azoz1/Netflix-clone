42a63c756ee848aca0e0a7ea946353f5b9fc4f95
Ahmed-Azoz1 <ahmedhjg123@gmail.com>
2024-03-31 12:15:32 +0200

    Add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,56 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
+
+
+const manifestForPlugin = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: 'Netflix-Clone',
+    short_name: 'Netflix-Clone',
+    theme_color: '#ffffff',
+    display:"standalone",
+    scope:'/',
+    start_url:"/",
+    orientation:'portrait',
+    icons: [
+        {
+            src: '/favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+        },
+        {
+            src: '/favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+        },
+        {
+            src: '/favicon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+        },
+        {
+            src: '/favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+        },
+        {
+            src: '/favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+        }
+    ],
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base:'./',
+  plugins: [react(),VitePWA(manifestForPlugin)],
 })
 
 
